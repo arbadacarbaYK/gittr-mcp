4eb849d6e18f7ca6371c57a9bc414ddf30e3dbb4
SatOpsHQ <sathq@tits4sats.com>
2026-02-02 19:10:32 +0000

    fix: Move nostr.wine to nip34Relays, remove damus/nos.lol
    - relay.noderunners.network has better NIP support for queries
    - nostr.wine returned 142 repos, keeping it in main list
    - Removed damus.io and nos.lol (not git-focused, rate limit issues)

--- config.js
+++ config.js
@@ -3,14 +3,15 @@ module.exports = {
   bridgeUrl: process.env.BRIDGE_URL || 'https://git.gittr.space',
   
   // NIP-34 aware relays (git-specific, prioritize these)
+  // Note: relay.noderunners.network has better NIP support than damus/nos.lol
   nip34Relays: (process.env.NIP34_RELAYS || 
-    'wss://relay.noderunners.network,wss://relay.ngit.dev,wss://git.shakespeare.diy,wss://ngit-relay.nostrver.se,wss://git-01.uid.ovh,wss://git-02.uid.ovh,wss://ngit.danconwaydev.com'
+    'wss://relay.noderunners.network,wss://relay.ngit.dev,wss://git.shakespeare.diy,wss://ngit-relay.nostrver.se,wss://git-01.uid.ovh,wss://git-02.uid.ovh,wss://ngit.danconwaydev.com,wss://nostr.wine'
   ).split(','),
   
-  // General Nostr relays (backup/redundancy)
+  // General Nostr relays (removed damus.io and nos.lol - not git-focused, rate limit issues)
   generalRelays: (process.env.GENERAL_RELAYS || 
-    'wss://relay.damus.io,wss://nos.lol,wss://nostr.wine'
-  ).split(','),
+    ''
+  ).split(',').filter(r => r.length > 0),
   
   // All relays combined (for backwards compatibility)
   get relays() {
